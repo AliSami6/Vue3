719ad184d394737852f2e25a44981f78d45d3e90
AliSami6 <alisamicse320@gmail.com>
2023-05-19 09:04:21 +0600

    using method

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ new Vue({
     el:"#app",
     data: {
         title :"Web And Vue",
+        desc :"long term course",
         learning_meta: "bootstrap5  Html5 css3 jquery javascript sql php",
         viva:['join sql','request life cycle'],
         written:{
             name: "written test 50 marks"
-        },
-        greeting:()=>{
-            return " You are selected";
+        }
+       
+    },
+    methods: {
+        greeting(){
+            return this.desc;
         }
     }
 })
