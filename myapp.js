4b0aa82a91c88c38ddc5310e7aa13a541e650135
AliSami6 <alisamicse320@gmail.com>
2023-05-19 17:24:17 +0600

    attribute binding in vue

---
+++ myapp.js
@@ -0,0 +1,8 @@
+new Vue({
+   el: "#app",
+   data: {
+      imageSrc:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dwburnett-watergap-boxster-miata-3919-1639685394.jpg?crop=1.00xw:0.752xh;0,0.149xh&resize=980:*',
+      alt:"this is toyota car",
+      link:"https://youtu.be/Lpe2HW8eNHM"
+   }
+});
