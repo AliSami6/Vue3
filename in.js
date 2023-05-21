981700875b1f35a9585504462a09045c06c05f7a
AliSami6 <alisamicse320@gmail.com>
2023-05-21 09:58:00 +0600

    how to create more instance

---
+++ in.js
@@ -0,0 +1,20 @@
+const app11 = new Vue({
+  el:"#app1",
+  data : {
+    value : '1st'
+  },
+  methods:{
+    changeValue(){
+       app12.value = 'changed';
+     }
+  },
+   
+});
+
+const app12 = new Vue({
+  el:"#app2",
+  data : {
+    value : '2nd'
+  },
+   
+});
