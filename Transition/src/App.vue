adfd79a3584910b01ad6a96bb4bfb7b6645d5d7d
AliSami6 <alisamicse320@gmail.com>
2023-06-05 11:43:47 +0600

    transition

---
+++ Transition/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <transition name="appear">
+        <div class="p-3 mb-3 bg-success text-white" v-if="display">transition</div>
+    </transition>
+    <button @click="display=!display" class="btn btn-primary btn-sm mt-3">Toggle</button>
+   
+  </div>
+</template>
+<script>
+
+
+export default ({
+  data(){
+    return {
+     display : false
+    }
+  }
+})
+</script>
+<style>
+  .appear-enter{
+    opacity:0;
+    transform: translateX(30px);
+  }
+  .appear-enter-active{
+    transition: all .3s ease;
+  }
+  .appear-leave{
+
+  }
+  .appear-leave-active{
+    opacity:0;
+    transform: translateX(30px);
+     transition: all .3s ease;
+  }
+  button{
+    border-radius: 50%;
+  }
+</style>
