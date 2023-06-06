038d00517a4c1cd8fcf1f8f4ee6968569364172a
AliSami6 <alisamicse320@gmail.com>
2023-06-06 10:27:47 +0600

    filters uppercase ,remove text

---
+++ filter/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <p class="text-center mt-4"> {{txt | uppercase }}</p>
+     <!--
+      <table>
+    <thead>
+      <th v-for="col in cols" :key="col">
+        {{col | uppercase}}
+      </th>
+    </thead>
+  </table>-->
+
+  </div>
+  
+</template>
+<script>
+
+
+export default ({
+  data(){
+    return {
+   txt: "Adnan Sami"
+  //  cols: ['nome', 'data', 'size', 'ext']
+    }
+  },
+  filters :{
+   uppercase: function(v) {
+      return v.toUpperCase();
+    },
+    removeText(value){
+      return value.slice(1);
+    }
+
+  }
+})
+</script>
+<style>
+  .appear-enter{
+    opacity:0;
+    transform: translateX(30px);
+  }
+  .appear-enter-active{
+    transition: all .3s ease;
+  }
+  .appear-leave{
+
+  }
+  .appear-leave-active{
+    opacity:0;
+    transform: translateX(30px);
+     transition: all .3s ease;
+  }
+  button{
+    border-radius: 50%;
+  }
+</style>
---
+++ filter/src/Components/ListHelper.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        
+         <slot name="listItem"></slot>
+          This is list helper
+          <slot name="para"></slot>
+          <slot name="other">Loading...</slot>
+    </div>
+   
+</template>
+<script>
+export default{
+    data(){
+        return {
+          
+        }
+    }
+}
+</script>
+<style scoped>
+ul li{
+    color:red;
+}
+p{
+    color:blue;
+}
+</style>
