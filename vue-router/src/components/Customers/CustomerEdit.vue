e83eb5f6c4e77e5577eb08627115de561ade3e18
AliSami6 <alisamicse320@gmail.com>
2023-06-13 11:43:16 +0600

    query parameter

--- vue-router/src/components/Customers/CustomerEdit.vue
+++ vue-router/src/components/Customers/CustomerEdit.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
         <h1>Customer Edit</h1>
+        <p>Local:{{ $route.query.local }}</p>
+        <p>Analytics:{{ $route.query.q }}</p>
     </div>
 </template>
--- vue-router/src/components/Customers/CustomerList.vue
+++ vue-router/src/components/Customers/CustomerList.vue
@@ -3,7 +3,7 @@
         <h1>Customer Lists {{ $route.params.id }}</h1>
         <router-link 
         tag="button"
-        :to="{name:'compCustomerEdit',params:{id:$route.params.id}}" class="btn btn-primary">
+        :to="{name:'compCustomerEdit',params:{id:$route.params.id},query:{local:'en',q:100}}" class="btn btn-primary">
        Edit User
         </router-link>
     </div>
